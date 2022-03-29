13167befdb3d3eb26fd4087e50dea819dc9bd5d3
luisgizirian <luisg@australnova.com>
2022-03-29 00:35:07 +0000

    cont'd from previous. Cleanup.

--- src/web-client/Services/Models.cs
+++ src/web-client/Services/Models.cs
@@ -1,5 +1,3 @@
 namespace WebClient.Services;
 
-public record WeatherForecast(DateTime Date, int TemperatureC, int TemperatureF, string? Summary);
-
 public record CatalogItem(string Id, string ProductName, string Sku, decimal Price, bool IsInStock);
