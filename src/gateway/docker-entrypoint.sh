293d5f35d1962cd0b4a32487fd3b1d5e007acfb1
luisgizirian <luisg@australnova.com>
2022-06-21 10:30:32 -0500

    Hiding Az Functions behind gateway + env fixes

--- src/gateway/docker-entrypoint.sh
+++ src/gateway/docker-entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "Generating envoy.yaml config file..."
-cat /tmpl/envoy.yaml.tmpl | envsubst \$ENVOY_CATALOG_API_ADDRESS,\$ENVOY_ORDERING_API_ADDRESS > /etc/envoy.yaml
+cat /tmpl/envoy.yaml.tmpl | envsubst \$ENVOY_CATALOG_API_ADDRESS,\$ENVOY_ORDERING_API_ADDRESS,\$ENVOY_FUNCTIONS_ADDRESS,\$ENVOY_CATALOG_API_PORT,\$ENVOY_ORDERING_API_PORT,\$ENVOY_FUNCTIONS_PORT > /etc/envoy.yaml
 
 echo "Starting Envoy..."
 /usr/local/bin/envoy -c /etc/envoy.yaml
